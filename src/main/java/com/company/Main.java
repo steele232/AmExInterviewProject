2374ef38a674daa91b9815795a326f47c5c5d686
steele232 <jonathan.steele232@gmail.com>
2020-07-22 13:54:14 -0700

    Step 1 Complete (with tests)
    	modified:   src/main/java/com/company/Main.java
    	modified:   src/test/java/com/company/MainTest.java

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,43 +2,115 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        // TODO optimize this with a string builder pattern.
-        System.out.println("Welcome to AmEx Groceries.");
-        System.out.println("Apples and Oranges are for sales.");
-        System.out.println("Here are today's Prices:");
-        System.out.println("- Apples: 60 cents");
-        System.out.println("- Oranges: 25 cents");
-        System.out.println("You may now order your items. ");
-        System.out.println("Please enter a comma-delimited list of items or an empty line if you don't want to purchase anything. ");
-        System.out.println("Here is an example: \"apple, apple, orange, orange\"");
-        System.out.println("Now please enter your order: ");
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Welcome to AmEx Groceries.\n");
+        sb.append("Apples and Oranges are for sales.\n");
+        sb.append("Here are today's Prices:\n");
+        sb.append("- Apples: 60 cents\n");
+        sb.append("- Oranges: 25 cents\n");
+        sb.append("You may now order your items. \n");
+        sb.append("Please enter a space-delimited list of items or an empty line if you don't want to purchase anything. \n");
+        sb.append("Here is an example: \"apple apple orange orange\"\n");
+        sb.append("Now please enter your order: ");
+        System.out.println(sb.toString());
+
         List<String> order = fetchOrderFromCLI();
 
+        // failure state: empty string as input
+        if (order.size() == 1 && order.get(0).equals("")) {
+            System.out.println("Empty input. Come back again later!");
+            return;
+        }
+
+        // failure state: product name not found
+        List<Long> prices = new ArrayList<>();
+        try {
+            for (String item : order) {
+                long priceInCents = getPriceInCents(item);
+                prices.add(priceInCents);
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getLocalizedMessage());
+            return;
+        }
+
+        System.out.println("Here is your order: ");
+        for (String item : order) {
+            String cost = null;
+            try {
+                cost = getFormattedPriceStringFromCents(getPriceInCents(item));
+            } catch (Exception e) {
+                // this should happen because getPriceInCents has already been called on all inputs
+                e.printStackTrace();
+                return;
+            }
+//            String cost
+            System.out.println("An " + item + " costs " + cost);
+        }
+
+        // print out the total in the right format
+        Long total = prices.stream().mapToLong(Long::longValue).sum();
+        String totalString = getFormattedPriceStringFromCents(total);
+        System.out.println("Here is your total price: " + totalString);
+
+        System.out.println("Thanks for shopping at AmEx Groceries. Have a nice day!");
     }
 
     private static List<String> fetchOrderFromCLI() {
         // fetch string
-        String userInput = ""; // TODO write this
+        Scanner cliScanner = new Scanner(System.in);
+        String userInput = cliScanner.nextLine();
 
         List<String> order = new ArrayList<>();
         order = parseOrderString(userInput);
         return order;
     }
 
-    // TODO
-    private static List<String> parseOrderString(String userInput) {
+    public static List<String> parseOrderString(String userInput) {
         // split on commas
+        String[] items = userInput.split(" ");
 
-        // trim and toLowerCase (Stream opportunity)
-
-        return null;
+        // trim and toLowerCase
+        return Stream.of(items)
+                .map(String::trim)
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
     }
 
-    public static long add(long a, long b) {
-        return a + b;
+    public static long getPriceInCents(String productName) throws Exception {
+        switch (productName) {
+            case "apple":
+                return 60l;
+            case "orange":
+                return 25l;
+            default:
+                throw new Exception("Product name not found: " + productName);
+        }
+    }
+
+    public static String getFormattedPriceStringFromCents(long price) {
+        long dollars = price / 100;
+        long cents = price % 100;
+        StringBuilder sb = new StringBuilder();
+        sb.append("$");
+        sb.append(dollars);
+        sb.append(".");
+        if (cents >= 10) {
+            sb.append(cents);
+        } else if (cents >= 1) {
+            sb.append("0");
+            sb.append(cents);
+        } else {
+            sb.append("00");
+        }
+        return sb.toString();
     }
 }
--- src/test/java/com/company/MainTest.java
+++ src/test/java/com/company/MainTest.java
@@ -1,17 +1,110 @@
 package com.company;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+
+    /**
+     * This is mainly for my personal benefit.
+     * Testing a stdlib method to verify its behavior
+     */
+    void splitString() {
+        System.out.println("Empty String: ");
+        String[] array = "".split(",");
+        printArray(array);
+        System.out.println("Size: " + array.length);
+
+        System.out.println("Single Apple String: ");
+        array = "apple".split(",");
+        printArray(array);
+        System.out.println("Size: " + array.length);
+
+        System.out.println("Two Apples String: ");
+        array = "apple, apple".split(",");
+        printArray(array);
+        System.out.println("Size: " + array.length);
+    }
+
+    void printArray(String[] strings) {
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
+
     @Test
-    void add() {
-        Assertions.assertEquals(4, Main.add(2, 2), "Main.add(2, 2) test failed");
-        long a = 10l;
-        long b = 20l;
-        Assertions.assertEquals(30l, Main.add(a,b));
+    void parseOrderString() {
+
+        String input = "apple apple Apple";
+        List<String> order = Main.parseOrderString(input);
+
+        List<String> expected = new ArrayList<>();
+        expected.add("apple");
+        expected.add("apple");
+        expected.add("apple");
+
+        Assertions.assertEquals(
+            expected,
+            order
+        );
+
+        input = "apple Orange apple";
+        order = Main.parseOrderString(input);
+        expected.clear();
+        expected.add("apple");
+        expected.add("orange");
+        expected.add("apple");
+
+        Assertions.assertEquals(
+                expected,
+                order
+        );
+
+    }
+
+    @Test
+    void getFormattedPriceStringFromCents() {
+        long price = 100l;
+        Assertions.assertEquals(
+                "$1.00",
+                Main.getFormattedPriceStringFromCents(price)
+        );
+
+        price = 120l;
+        Assertions.assertEquals(
+                "$1.20",
+                Main.getFormattedPriceStringFromCents(price)
+        );
+
+        price = 20l;
+        Assertions.assertEquals(
+                "$0.20",
+                Main.getFormattedPriceStringFromCents(price)
+        );
+
+        price = 5l;
+        Assertions.assertEquals(
+                "$0.05",
+                Main.getFormattedPriceStringFromCents(price)
+        );
+
+        price = 0l;
+        Assertions.assertEquals(
+                "$0.00",
+                Main.getFormattedPriceStringFromCents(price)
+        );
+
+        price = 1020l;
+        Assertions.assertEquals(
+                "$10.20",
+                Main.getFormattedPriceStringFromCents(price)
+        );
     }
 }
